f0318df73f2c4ded1da9a01bc76c8d1a22d42295
goaregua <goaregua@INFO2-15>
2019-04-10 14:35:04 +0200

    Ajoutez des fichiers projet.

---
+++ DS_ArthurGOAREGUER/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DS_ArthurGOAREGUER.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
